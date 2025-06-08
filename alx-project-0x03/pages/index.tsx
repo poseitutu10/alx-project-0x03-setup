3bf3c10779f7b1cc1e636031945cc90f3f4124ba
Phinehas Osei-Tutu <phinehasoseitutu1@gmail.com>
2025-06-08 22:21:26 +0000

    imperative routing

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,17 +1,39 @@
+import Button from "@/components/common/Button";
 import Layout from "@/components/layouts/Layout";
+import { useRouter } from "next/router";
 import React from "react";
 
 
+interface pageRouteProps {
+  pageRoute: string;
+}
+
 const Home: React.FC = () => {
+
+  const router = useRouter()
+
+  const routeToNextPage = ( { pageRoute }: pageRouteProps) => {
+    router.push(pageRoute, undefined, {shallow: false} )
+  }
   return (
-    <>
-    <Layout>
-      <div className="h-screen pt-28 ">
-        <h1> Welcome to Splash App </h1>
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
+      {/* Welcome Message */}
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Welcome to Splash App!
+      </h1>
+      <p className="text-lg text-gray-600 mb-8">
+        Your one-stop platform for everything AI you need. Start exploring by
+        navigating to our features below.
+      </p>
+
+      {/* Navigation Options */}
+      <div className="flex gap-6">
+        <Button action={() => routeToNextPage({ pageRoute: '/generate-text-ai' })} buttonLabel="Generate Text" backgroundColor="blue" />
+        <Button action={() => routeToNextPage({ pageRoute: '/text-to-image'})} buttonLabel="Text to Image" backgroundColor="green" />
+        <Button action={() => routeToNextPage({ pageRoute: '/counter-app'})} buttonLabel="Contact us" backgroundColor="orange" />
       </div>
-    </Layout>
-    </>
-  )
+    </div>
+  );
 }
 
 export default Home;
