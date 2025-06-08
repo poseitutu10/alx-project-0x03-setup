29aa155cd27780772d9f1c0022de74194f59eb1f
Phinehas Osei-Tutu <phinehasoseitutu1@gmail.com>
2025-06-08 17:21:28 +0000

    layout in nextjs

---
+++ alx-project-0x03/interfaces/index.ts
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  backgroundColor?: "red" | "blue" | "orange" | "green";
+  action?: () => void;
+}
+
+export interface LayoutProps {
+  children: ReactNode
+}
---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,19 @@
+import Footer from "@/components/layouts/Footer";
+import Header from "@/components/layouts/Header";
+import Layout from "@/components/layouts/Layout";
+import React from "react";
+
+
+const Home: React.FC = () => {
+  return (
+    <>
+    <Layout>
+      <div className="h-screen pt-28 ">
+        <h2>Main Page</h2>
+      </div>
+    </Layout>
+    </>
+  )
+}
+
+export default Home;
